d5753827fd82b8d294de048ef26303381325f98f
DSM1 <fatec-dsm1@academico.intra>
2024-06-26 21:49:34 -0300

    exexercicios

--- logica2-js/js-curso-2-aula_1/exercicios/03/app.js
+++ logica2-js/js-curso-2-aula_1/exercicios/03/app.js
@@ -1,2 +1,7 @@
 //Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
-let numero = 
+let numero = prompt('escolha um numero')
+function gerarDobro(numero){
+    return 2*numero;
+}
+let resultado=gerarDobro(numero);
+console.log(resultado);
---
+++ logica2-js/js-curso-2-aula_1/exercicios/04/app.js
@@ -0,0 +1,25 @@
+//Criar uma função que recebe três números como parâmetros e retorna a média deles.
+function exibirTexto(tag,texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+exibirTexto('h1','exercicio 04');
+exibirTexto('p','Criar uma função que recebe três números como parâmetros e retorna a média deles.');
+
+function Vlr1(){
+   let valor1 = prompt('porfavor informar o valor do numero');
+    console.log(valor1);
+}
+function Vlr2(){
+    let valor2 = prompt('porfavor informar o valor do numero');
+    console.log(valor2);
+}
+function Vlr3(){
+   let valor3 = prompt('porfavor informar o valor do numero');
+console.log(valor3);
+}
+
+function mediaVlr(valor1, valor, valor3){
+    
+console.log(valor1+valor2+valor3);       
+}    
